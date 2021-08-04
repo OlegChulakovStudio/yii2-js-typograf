8ed76199cf7a61904f2c202bbde41c2d85665534
Алексей Палагичев <alexey.palagichev@chulakov.ru>
2021-08-04 13:46:14 +0300

    Обновил assets

---
+++ src/assets/TinyMceAssets.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @copyright Copyright (c) 2020, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+namespace chulakov\typograf\assets;
+
+use yii\web\AssetBundle;
+
+class TinyMceAssets extends AssetBundle
+{
+    /**
+     * Стили CSS
+     */
+    const CONTENT_CSS = '/css/common.css';
+
+    /**
+     * @var string
+     */
+    public $sourcePath = '@vendor/chulakov/yii2-js-typograf/src/assets';
+
+    /**
+     * @var array
+     */
+    public $css = [
+        'css/common.css',
+    ];
+
+    /**
+     * @var array
+     */
+    public $js = [
+        'js/typograf/typograf.all.min.js',
+        'js/typografTinyMCE.js',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public $depends = [
+        'dosamigos\tinymce\TinyMceAsset',
+    ];
+}
